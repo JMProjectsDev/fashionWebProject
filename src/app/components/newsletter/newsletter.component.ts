5017549c6d7b96518083b22fcd8f87827c3e5764
JMProyects <jacquesmendieta@gmail.com>
2024-01-23 15:57:07 +0100

    Fixed responsiveness top-ventas component. Checking input validators with regex (newsletter component)

--- src/app/components/newsletter/newsletter.component.ts
+++ src/app/components/newsletter/newsletter.component.ts
@@ -8,22 +8,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class NewsletterComponent {
   formulario: FormGroup;
-
-  constructor(private fb: FormBuilder) {
+  registroExitoso: Boolean = false;
+  constructor(private fb: FormBuilder) {    
     this.formulario = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+        ],
+      ],
     });
   }
 
   checkEmail() {
     if (this.formulario.valid) {
+      //enviar datos backend
+      this.registroExitoso = true
       console.log('Email correcto!');
     } else {
+      //controlar errores
+      this.registroExitoso = false
       console.log('Email incorrecto!');
     }
 
-    Object.values(this.formulario.controls).forEach(
-      (control) => control.markAsTouched
+    Object.values(this.formulario.controls).forEach((control) =>
+      control.markAsTouched()
     );
   }
 }
