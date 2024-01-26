6117cbaa6b808f5cb1a3eea98f6c58c47775102f
JMProyects <jacquesmendieta@gmail.com>
2024-01-26 20:26:16 +0100

    Implementing email validator

--- src/app/components/newsletter/newsletter.component.ts
+++ src/app/components/newsletter/newsletter.component.ts
@@ -8,7 +8,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class NewsletterComponent {
   formulario: FormGroup;
-  email: string = ''
+  email: string = '';
   registroExitoso: Boolean = false;
   constructor(private fb: FormBuilder) {
     this.formulario = this.fb.group({
@@ -22,25 +22,24 @@ export class NewsletterComponent {
     });
   }
 
-  checkEmail() {
-    if (this.formulario.valid) {
+  checkEmail(email: string) {
+    if (this.formulario.valid && email !== null) {
       console.log('Email correcto!');
       this.registroExitoso = true;
       // Enviar datos al backend...
-  
+
       // Resetear el formulario pero no limpiar explícitamente los errores
       this.formulario.reset();
     } else {
       console.log('Email incorrecto!');
       this.registroExitoso = false;
     }
-  
+
     // Marcar como 'touched' solo si el formulario es inválido para mostrar errores
     if (this.formulario.invalid) {
-      Object.values(this.formulario.controls).forEach(control => {
+      Object.values(this.formulario.controls).forEach((control) => {
         control.markAsTouched();
       });
     }
   }
-  
 }
