45959ef2f6512f5d865d64e51199aaa937224762
JMProyects <jacquesmendieta@gmail.com>
2025-01-12 16:57:14 +0100

    Add new arrivals products view at search comp.

--- src/app/newsletter/components/newsletter/newsletter.component.ts
+++ src/app/newsletter/components/newsletter/newsletter.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertService } from 'src/app/shared/services/alert.service';
 
--- src/app/search/components/search/search.component.ts
+++ src/app/search/components/search/search.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  searchForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      searchTerm: ['', [Validators.required]],
+    });
+
+    this.searchForm.valueChanges.subscribe(() => {
+      console.log('Valor de búsqueda:', this.searchForm.get('searchTerm')?.value);
+    });
+  }
+
+  performSearch() {
+    if (this.searchForm.valid) {
+      const searchTerm = this.searchForm.get('searchTerm')?.value;
+
+      console.log(`Realizando búsqueda por: ${searchTerm}`);
+    }
+  }
+
+  clearSearch() {
+    this.searchForm.reset();
+  }
 }
